c32e6abbbf06265cff0079de3aaa1cec8d17701d
Joey Sacchini <joey@sacchini.net>
2020-10-20 21:42:07 -0400

    add feature flags for the different version modules

--- src/lib.rs
+++ src/lib.rs
@@ -7,17 +7,20 @@ extern crate alloc;
 extern crate test;
 
 mod deserialize;
+mod serialize;
 pub mod nbt;
 pub mod protocol;
-mod serialize;
 pub mod status;
 pub mod types;
 pub mod utils;
 pub mod uuid;
-pub mod v1_15_2;
-pub mod v1_16_3;
 mod chat;
-mod byte_order;
+pub mod byte_order;
+
+#[cfg(feature = "v1_15_2")]
+pub mod v1_15_2;
+#[cfg(feature = "v1_16_3")]
+pub mod v1_16_3;
 
 pub use deserialize::*;
 pub use serialize::*;
